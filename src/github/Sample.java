93912ea0a04b840e9594e03a3f02a04b48345734
YuvarajSekar3 <yuvi.sekar3@gmail.com>
2019-02-11 21:14:13 +0530

    baseclass

---
+++ src/github/Sample.java
@@ -0,0 +1,16 @@
+package github;
+
+import org.testng.annotations.Test;
+
+public class Sample {
+	
+	@Test
+	private void emp() {
+	System.out.println("yuvaraj");
+}
+	@Test
+	private void empId() {
+		System.out.println("sai");
+	}
+
+}
